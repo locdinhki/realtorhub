fbff44ea2acf55ec27ba70a27b5e2291e48d9a48
Vincent Thieu <vvt100020@gmail.com>
2018-10-12 23:31:01 -0500

    add code model

---
+++ RealtorHub/Models/CodeModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace RealtorHub.Models
+{
+    public enum CodeTier
+    {
+        COPPER,
+        SILVER,
+        GOLD,
+        PLATINUM,
+        DIAMOND
+    }
+    public class CodeModel
+    {
+        public class Code
+        {
+            public int ID { get; set; }
+            public string Value { get; set; }
+            public int DiscountTier { get; set; }
+            public double[] DiscountRate = { .08, .1, .12, .15, .2 };
+        }
+
+    }
+}
