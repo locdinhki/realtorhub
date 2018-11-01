bab74eb7e870240fd7b9b1a39e8307edaa66b94c
locdinhki <vvt100020@gmail.com>
2018-10-31 22:22:11 -0700

    Add user model

---
+++ RealtorHub/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealtorHub.Models
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        private string password;
+
+        private string GetPassword()
+        {
+            return password;
+        }
+
+        private void SetPassword(string value)
+        {
+            password = value;
+        }
+    }
+}
